721b3ce97850922a35d645bf9f7bc63450175ea8
dijon <dijonmboko@gmail.com>
2026-04-10 12:41:31 +0200

    model and migrations added

--- API/Program.cs
+++ API/Program.cs
@@ -1,23 +1,36 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+
+});
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope(); // Create a scope to get the services we need to seed the database with some initial data
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<AppDbContext>(); // Get the database context from the service provider
+    // await context.Database.MigrateAsync(); // Apply any pending migrations to the database
+    await DbInitializer.SeedData(context); // Seed the database with some initial data
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred seeding the DB.");
+}
 
 app.Run();
---
+++ Persistence/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Common;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence;
+
+public class AppDbContext(DbContextOptions options): DbContext(options)
+{
+    public required DbSet<Activity> Activities { get; set; }
+}
