f916317f67755133aaca2cf0a5c6e3251ee9cee6
dijon <dijonmboko@gmail.com>
2026-04-17 13:05:55 +0200

    cors middleware added, client site added, axios added for http request

--- API/Program.cs
+++ API/Program.cs
@@ -9,13 +9,16 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-
 });
+
+builder.Services.AddCors();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+// These are middlewares configurations.
+app.UseCors(option => option.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:5173", "https://localhost:5173"));
 
 app.MapControllers();
 
